d2a468c22c25447ac51da9d6468983a67250ea6c
afe-e <afetrabu@gmail.com>
2026-05-06 13:50:17 +0200

    AllCardList

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CardList } from './card-list/card-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: CardList}
+];
--- src/app/services/tgcp-api.ts
+++ src/app/services/tgcp-api.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
+import TCGdex from '@tcgdex/sdk';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TgcpApi {}
+
+export class TgcpApi {
+  private readonly tcgdex = new TCGdex('en');
+  async getAllCards() {
+    return await this.tcgdex.card.list();
+  }
+}
